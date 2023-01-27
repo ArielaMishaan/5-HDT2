9d68ec3e56dc0248e0c7d455c47743f83bb46c5f
CARPTatj <car22539@uvg.edu.gt>
2023-01-27 10:22:21 -0600

    JUNIT

--- Test/CalcTest.java
+++ Test/CalcTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 /*
@@ -17,121 +20,72 @@ class CalcTest {
 	int numero = 4;
 
 	@Test
-	void pullTest() {
+	void pullpushTest() {
+		StackHandmade stack = new StackHandmade<Integer>();
 		stack.push(4);
-		double estacion = miRadio.getFMActualStation();
-		assertEquals(88.10000000000001, estacion);
+		stack.push(3);
+		stack.push(2);
+		int resultado = 0;
+		resultado = (int) stack.pull();
+		assertEquals(2, resultado);
 	}
 
 	@Test
-	void onTest(){
-		miRadio = new Radio();
-		miRadio.on();
-		assertEquals(true, miRadio.isOn());
+	void sumaTest() throws Exception{
+		Calculadora calculadora = new Calculadora();
+		ArrayList<String> lista =  new ArrayList<>();
+		lista.add("2");
+		lista.add("3");
+		lista.add("+");
+		int resultado = calculadora.Calculate(lista);
+		assertEquals(5, resultado);
 	}
 
 	@Test
-	void offTest(){
-		miRadio = new Radio();
-		miRadio.off();
-		assertEquals(false, miRadio.isOn());
-	}
-
-	
-	/** 
-	 * @throws Exception
-	 */
-	@Test
-	void setFrequenceTest() throws Exception{
-		miRadio = new Radio();
-		miRadio.setFrequence("AM");
-		assertEquals("AM", miRadio.getFrequence());
+	void restaTest() throws Exception{
+		Calculadora calculadora = new Calculadora();
+		ArrayList<String> lista =  new ArrayList<>();
+		lista.add("3");
+		lista.add("2");
+		lista.add("-");
+		int resultado = calculadora.Calculate(lista);
+		assertEquals(-1, resultado);
 	}
 
 	@Test
-	void getFrequenceTest(){
-		miRadio = new Radio();
-		miRadio.getFrequence();
-		assertEquals("FM", miRadio.getFrequence());
+	void multiTest() throws Exception{
+		Calculadora calculadora = new Calculadora();
+		ArrayList<String> lista =  new ArrayList<>();
+		lista.add("7");
+		lista.add("9");
+		lista.add("*");
+		int resultado = calculadora.Calculate(lista);
+		assertEquals(63, resultado);
 	}
 
 	@Test
-	void ForwardAMTest(){
-		miRadio = new Radio();
-		miRadio.Forward();
-		assertEquals(540, miRadio.getAMActualStation());
+	void diviTest() throws Exception{
+		Calculadora calculadora = new Calculadora();
+		ArrayList<String> lista =  new ArrayList<>();
+		lista.add("6");
+		lista.add("24");
+		lista.add("/");
+		int resultado = calculadora.Calculate(lista);
+		assertEquals(4, resultado);
 	}
 
 	@Test
-	void BackwardAMTest(){
-		miRadio = new Radio();
-		miRadio.Backward();
-		assertEquals(1610, miRadio.getAMActualStation());
-	}
-	
-	@Test
-	void BackwardFMTest(){
-		miRadio = new Radio();
-		miRadio.setFMActualStation(88.3);
-		miRadio.Backward();
-		assertEquals(88.1, miRadio.getFMActualStation());
-	}
-
-	@Test
-	void getFMActualStationTest(){
-		miRadio = new Radio();
-		miRadio.getFMActualStation();
-		assertEquals(87.9, miRadio.getFMActualStation());
-	}
-
-	@Test
-	void getAMActualStationTest(){
-		miRadio = new Radio();
-		miRadio.getAMActualStation();
-		assertEquals(530, miRadio.getAMActualStation());
-	}
-
-	@Test
-	void setFMActualStationTest(){
-		miRadio = new Radio();
-		miRadio.setFMActualStation(89.1);
-		assertEquals(89.1, miRadio.getFMActualStation());
-	}
-
-	@Test
-	void setAMActualStationTest(){
-		miRadio = new Radio();
-		miRadio.setAMActualStation(550);
-		assertEquals(550, miRadio.getAMActualStation());
-	}
-
-	@Test
-	void saveFMStationTest(){
-		miRadio = new Radio();
-		miRadio.saveFMStation(89.1, 2);
-		assertEquals(89.1, miRadio.getFMSlot(2));
-	}
-
-	@Test
-	void saveAMStationTest(){
-		miRadio = new Radio();
-		miRadio.saveAMStation(600, 6);
-		assertEquals(600, miRadio.getAMSlot(6));
-	}
-
-	@Test
-	void getFMSlotTest(){
-		miRadio = new Radio();
-		miRadio.saveFMStation(89.7, 6);
-		miRadio.getFMSlot(6);
-		assertEquals(89.7, miRadio.getFMSlot(6));
-	}
-
-	@Test
-	void getAMSlotTest(){
-		miRadio = new Radio();
-		miRadio.saveAMStation(610, 6);
-		miRadio.getAMSlot(6);
-		assertEquals(610, miRadio.getAMSlot(6));
+	void unmontonTest() throws Exception{
+		Calculadora calculadora = new Calculadora();
+		ArrayList<String> lista =  new ArrayList<>();
+		lista.add("1");
+		lista.add("2");
+		lista.add("+");
+		lista.add("4");
+		lista.add("*");
+		lista.add("3");
+		lista.add("+");
+		int resultado = calculadora.Calculate(lista);
+		assertEquals(15, resultado);
 	}
 }
